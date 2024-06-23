54da7d9be955ff50a5054d4406f5b7369cb08986
Volodymyr Dombrovskyi <dombrovsky@users.noreply.github.com>
2024-06-23 14:09:24 +0200

    add terraform template

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HSA11HW26
+
+terraform init
+
+$Env:AWS_ACCESS_KEY_ID = 'anaccesskey'
+$Env:AWS_SECRET_ACCESS_KEY = 'asecretkey'
+
+terraform plan
+
+terraform apply
---
+++ main.tf
@@ -0,0 +1,178 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
+
+resource "aws_instance" "instance1" {
+  ami                   = data.aws_ami.amazon_linux.id
+  instance_type         = "t2.micro"
+  subnet_id             = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              echo "instance1" > /usr/share/nginx/html/index.html
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "instance1"
+  }
+}
+
+resource "aws_instance" "instance2" {
+  ami                   = data.aws_ami.amazon_linux.id
+  instance_type         = "t2.micro"
+  subnet_id             = aws_subnet.subnet2.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              echo "instance2" > /usr/share/nginx/html/index.html
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "instance2"
+  }
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_ssh_http.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "app-lb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    path                = "/"
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "app-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance1" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.instance1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "instance2" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.instance2.id
+  port             = 80
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.app_lb.dns_name
+}
